4cac05010d0e0e649a59c789b6f277ef5f9f947a
Sean van Wyk <sean.vanwyk@bbd.co.za>
2024-05-02 17:03:28 +0200

    Fix formatting

--- terraform/main/vpc.tf
+++ terraform/main/vpc.tf
@@ -22,8 +22,8 @@ module "vpc" {
 module "vpc_sg" {
   source = "terraform-aws-modules/security-group/aws"
 
-  name        = "spidey-speed-dating-sg"
-  vpc_id      = module.vpc.vpc_id
+  name              = "spidey-speed-dating-sg"
+  vpc_id            = module.vpc.vpc_id
   security_group_id = module.vpc.default_security_group_id
 
   ingress_with_cidr_blocks = [
