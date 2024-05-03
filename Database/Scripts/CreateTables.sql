220f6adac0a07d312ea5fe1b23342d14e6e5e0de
MitchdeKock <159277915+MitchdeKock@users.noreply.github.com>
2024-05-03 18:10:47 +0200

    Further db things

--- Database/Scripts/CreateTables.sql
+++ Database/Scripts/CreateTables.sql
@@ -1,29 +1,32 @@
 CREATE TABLE "User" (
-  user_id SERIAL PRIMARY KEY,
-  auth_id VARCHAR(50) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  username VARCHAR(50),
-  Gender VARCHAR(50),
-  Sexuality VARCHAR(50),
-  Age INT
+  "user_id" serial PRIMARY KEY,
+  "auth_id" varchar(50) NOT NULL,
+  "email" varchar(255) NOT NULL,
+  "username" varchar(50),
+  "Gender" varchar(50),
+  "Sexuality" varchar(50),
+  "Age" int
 );
 
 CREATE TABLE "UserQuestion" (
-  user_question_id SERIAL PRIMARY KEY,
-  user_id INT NOT NULL,
-  question_id INT NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES "User"(user_id),
-  FOREIGN KEY (question_id) REFERENCES "Question"(question_id)
+  "user_question_id" serial PRIMARY KEY,
+  "user_id" int NOT NULL,
+  "question_id" int NOT NULL
 );
 
 CREATE TABLE "Question" (
-  question_id SERIAL PRIMARY KEY,
-  question VARCHAR(500),
-  answer_id INT NOT NULL,
-  FOREIGN KEY (answer_id) REFERENCES "Answer"(answer_id)
+  "question_id" serial PRIMARY KEY,
+  "question" varchar(500)
 );
 
 CREATE TABLE "Answer" (
-  answer_id SERIAL PRIMARY KEY,
-  answer VARCHAR(500)
+  "answer_id" serial PRIMARY KEY,
+  "answer" varchar(500),
+  "question_id" int NOT NULL
 );
+
+ALTER TABLE "UserQuestion" ADD FOREIGN KEY ("user_id") REFERENCES "User" ("user_id");
+
+ALTER TABLE "UserQuestion" ADD FOREIGN KEY ("user_question_id") REFERENCES "Question" ("question_id");
+
+ALTER TABLE "Answer" ADD FOREIGN KEY ("question_id") REFERENCES "Question" ("question_id");
---
+++ database/scripts/SeedData.sql
@@ -0,0 +1,17 @@
+INSERT INTO "Question" ("question") VALUES
+  ('What are your hobbies or interests?'),
+  ('What''s your favorite type of music or artist?'),
+  ('What''s the last book you read or movie you watched that you really enjoyed?'),
+  ('What''s your idea of a perfect date?'),
+  ('What do you value most in a relationship?'),
+  ('What''s something you''re passionate about?'),
+  ('What''s a skill you''d like to learn or improve?'),
+  ('Do you prefer outdoor activities or indoor activities?'),
+  ('What''s your favorite way to spend a weekend?'),
+  ('What''s your favorite food or cuisine?'),
+  ('What''s your favorite travel destination or dream vacation spot?'),
+  ('What''s something that always makes you laugh?'),
+  ('What''s your biggest pet peeve?'),
+  ('What''s a goal you''re currently working towards?'),
+  ('What''s a cause or charity you care about?'),
+  ('What''s your idea of happiness?');
