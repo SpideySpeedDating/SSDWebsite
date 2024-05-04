8577071d5c92e73324013cfc8f52cb1f3dcba1e6
Mamphane Edwin Mokoena <mamphane@bbd.co.za>
2024-05-04 16:01:14 +0200

    mosaic lib in js

---
+++ frontend/lib/router.js
@@ -0,0 +1,51 @@
+import { Utils } from "./utils.js";
+class Router {
+    routes = null;
+    routeHistory = [];
+    static default404 = {
+        title: "404 Not Found",
+        template: "/templates/default-404.html"
+    };
+
+    constructor(routes, state = {}) {
+        this.routes = routes;
+        this.registerState(state);
+        let uriHandlerClosure = Router.getRouterBoundUriHandler(this);
+        window.addEventListener("hashchange", uriHandlerClosure);
+        uriHandlerClosure();
+    }
+    registerState(state) {
+        if (Utils.isNullOrUndefined(window.MosaicState))
+            window.MosaicState = {};
+        else
+            window.MosaicState = { ...window.MosaicState, ...state };
+    }
+    getRoute() {
+        let location = window.location.hash.replace("#", "");
+        if (location.length == 0)
+            location = "/";
+        return this.routes?.[location] || this.routes?.["404"] || Router.default404;
+    }
+
+    static getRouterBoundUriHandler(router) {
+        return async (e) => {
+            if (!Utils.isNullOrUndefined(e)) e.preventDefault();
+            let route = router.getRoute();
+            router.routeHistory.push(route);
+            router.updateHtml(await router.getTemplateHtml(route.template), route.title);
+        };
+    }
+    async getTemplateHtml(templatePath) {
+        return await fetch(`${window.location.origin}/frontend${templatePath}`).then((response) => response.text());
+    }
+    mountStyleTag() {
+    }
+    mountScriptTag() {
+    }
+    updateHtml(innerHtml, title) {
+        let appContainer = document.getElementById("app-container");
+        appContainer.innerHTML = innerHtml;
+        document.title = title;
+    }
+}
+export { Router };
