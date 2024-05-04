bfe2119d2da50c0cf0757e33623f633e3bba28e9
Sean van Wyk <sean.vanwyk@bbd.co.za>
2024-05-04 09:36:27 +0200

    Update DB scripts

--- database/scripts/v1.2__SeedData.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO question (question) VALUES
-  ('Were you breastfed as a child?'),
-  ('What type of bear is best?'),
-  ('Describe your perfect avocado?'),
-  (' What is your opinion on the following statement: Butter is food lotion'),
-  ('How did Bruno Mars know her eyes were open, unless his were too?'),
-  ('Do you agree with the following statement: Male spiders can be romantic'),
-  ('If you were stuck on a deserted island, and you could only write one Javascript program to keep you busy, how would you kill yourself?'),
-  ('Would you rather be stuck on an island with a male spider or a bear spider?');
