1028722baf69bdcdccf16b482cdcbcf3acb6a9e9
Mamphane Edwin Mokoena <mamphane@bbd.co.za>
2024-05-04 15:57:28 +0200

    Added index page route in index.js

--- frontend/index.js
+++ frontend/index.js
@@ -1,4 +1,8 @@
 import { Router } from "./lib/router.js";
 
-const router = new Router({}, {});
-await router.run();
+const router = new Router({
+    "/" : {
+        title: "Home",
+        template: "/templates/index.html"
+    }
+}, {});
