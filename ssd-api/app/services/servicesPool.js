8da5c5bfa11ab470bc109c415ec66d7f115554c8
MitchdeKock <159277915+MitchdeKock@users.noreply.github.com>
2024-05-09 13:42:51 +0200

    moved db connection info to env

--- ssd-api/app/services/servicesPool.js
+++ ssd-api/app/services/servicesPool.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'SpideySpeedDating',
-  password: 'admin',
-  port: 5432, // default PostgreSQL port
+  user: process.env.DB_USER,
+  host: process.env.DB_CONNECTIONSTRING,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT
 });
 
 module.exports = pool;
